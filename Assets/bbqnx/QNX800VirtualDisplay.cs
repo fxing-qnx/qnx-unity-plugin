9770d3c6ed7bd012eb02df7d670354ed074042b6
Felix Xing <fxing@qnx.com>
2025-03-06 16:52:44 -0500

    add event and debug display now

--- Assets/bbqnx/QNX800VirtualDisplay.cs
+++ Assets/bbqnx/QNX800VirtualDisplay.cs
@@ -1,37 +1,51 @@
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using System;
 using System.Runtime.InteropServices;
 
-[CreateAssetMenu(fileName = "QNX800VirtualDisplayLoader", menuName = "bbqnx-test/QNX800 Virtual Screen Loader")]
-public class QNX800VirtualDisplayLoader : ScriptableObject
+[CreateAssetMenu(fileName = "QNX800VirtualDisplay", menuName = "bbqnx-test/QNX800 Virtual Screen Loader")]
+public class QNX800VirtualDisplay : ScriptableObject
 {
     [DllImport ("bbqnx/QNX800VirtualDisplayPlugin")]
-    private static extern UInt64 NewQNX800VirtualDisplay (UInt32 width, UInt32 height);
+    private static extern UInt64 NewQNX800VirtualDisplay(Int32 width, Int32 height);
 
     [DllImport ("bbqnx/QNX800VirtualDisplayPlugin")]
-    private static extern void DelQNX800VirtualDisplay (UInt64 handle);
+    private static extern void DelQNX800VirtualDisplay(UInt64 handle);
     
     [DllImport ("bbqnx/QNX800VirtualDisplayPlugin")]
-    private static extern UInt64 ReadQNX800VirtualDisplay (UInt64 VirtualDisplayHandle, UInt32 GLTexHandle);
+    private static extern void ReadQNX800VirtualDisplay(UInt64 virtualDisplayHandle, Int32 width, Int32 height, UInt64 pixels);
 
     private UInt64 m_VirtualDisplayHandle = 0;
+    private Texture2D m_TargetTex = null;
 
-    public void NewVirtualDisplay (UInt32 width, UInt32 height)
+    public void NewVirtualDisplay(Int32 width, Int32 height)
     {
+        m_VirtualDisplayHandle = NewQNX800VirtualDisplay(width, height);
+
         if(m_VirtualDisplayHandle != 0)
         {
-            m_VirtualDisplayHandle = NewQNX800VirtualDisplay (width, height);
+            m_TargetTex = new Texture2D (width, height);
         }
     }
 
-    public void OnDestroy ()
+    public void OnDestroy()
     {
-        DelQNX800VirtualDisplay (m_VirtualDisplayHandle);
+        DelQNX800VirtualDisplay(m_VirtualDisplayHandle);
     }
 
-    public UInt64 ReadVirtualDisplay (UInt32 width, UInt32 height)
+    public void ReadVirtualDisplay ()
     {
-        return ReadQNX800VirtualDisplay (m_VirtualDisplayHandle, 78);
+        NativeArray<Color32> pixels = m_TargetTex.GetRawTextureData<Color32>();
+        unsafe 
+        {
+            ReadQNX800VirtualDisplay(m_VirtualDisplayHandle, 0, 0, (UInt64) pixels.GetUnsafePtr());
+        }
+    }
+
+    public Texture2D GetScreenTexture()
+    {
+        return m_TargetTex;
     }
 }
 
--- Assets/bbqnx/QNX800VirtualDisplayPlugin.cpp
+++ Assets/bbqnx/QNX800VirtualDisplayPlugin.cpp
@@ -2,8 +2,8 @@
 #include <cstdint>
 #include <unordered_map>
 
-#include <screen.h>
 #include <pthread.h>
+#include <screen/screen.h>
 #include <qh/error.h>
 
 #include "IUnityInterface.h"
@@ -16,67 +16,123 @@ static screen_context_t ctx = nullptr;
 static screen_display_mode_t mode = {};
 static std::unordered_map<screen_display_t, screen_buffer_t> buffers = {};
 
-extern "C"
+struct clean_up
 {
-    void UIX UnityPluginLoad(IUnityInterfaces* unityInterfaces)
+    ~clean_up() {}
+} clean_up;
+
+extern "C" void UIX UnityPluginLoad(IUnityInterfaces* unityInterfaces)
+{
+    unity_itx = unityInterfaces;
+    logger = unity_itx->Get<IUnityLog>();
+
+    // create screen ctx
+    if (screen_create_context(&ctx, SCREEN_DISPLAY_MANAGER_CONTEXT))
     {
-        unity_itx = unityInterfaces;
-        logger = unity_itx->Get<IUnityLog>();
-
-        // create screen ctx
-        if (screen_create_context(&ctx, SCREEN_DISPLAY_MANAGER_CONTEXT))
-        {
-            UNITY_LOG(logger, qh_strerror(errno));
-        }
-
-        mode.refresh = 60;
-        mode.index = 0;
-        mode.format = SCREEN_FORMAT_RGBA8888;
-        mode.flags = SCREEN_DISPLAY_MODE_PREFERRED |    //
-                     SCREEN_DISPLAY_MODE_FORMAT_VALID | //
-                     SCREEN_DISPLAY_MODE_REFRESH_VALID;
+        UNITY_LOG(logger, qh_strerror(errno));
     }
 
-    void UIX UnityPluginUnload(IUnityInterfaces* unityInterfaces)
+    mode.refresh = 60;
+    mode.index = 0;
+    mode.format = SCREEN_FORMAT_RGBA8888;
+    mode.flags = SCREEN_DISPLAY_MODE_PREFERRED |    //
+                 SCREEN_DISPLAY_MODE_FORMAT_VALID | //
+                 SCREEN_DISPLAY_MODE_REFRESH_VALID;
+}
+
+extern "C" void UIX UnityPluginUnload(IUnityInterfaces* unityInterfaces)
+{
+    if (ctx && screen_destroy_context(ctx))
     {
-        if (ctx && screen_destroy_context(ctx))
-        {
-            UNITY_LOG(logger, qh_strerror(errno));
-        }
+        UNITY_LOG(logger, qh_strerror(errno));
+    }
+}
+
+extern "C" std::uint64_t UIX NewQNX800VirtualDisplay(std::int32_t width, std::int32_t height)
+{
+    if (!ctx)
+    {
+        return reinterpret_cast<std::uint64_t>(nullptr);
     }
 
-    std::uint64_t UIX NewQNX800VirtualDisplay(std::uint32_t width, std::uint32_t height)
+    screen_display_t v_display = nullptr;
+    mode.width = width;
+    mode.height = height;
+    mode.aspect_ratio[0] = width;
+    mode.aspect_ratio[1] = height;
+
+    if (screen_create_display(&v_display, ctx, &mode))
     {
-        if (!ctx)
-        {
-            return reinterpret_cast<std::uint64_t>(nullptr);
-        }
-
-        screen_display_t v_display = nullptr;
-        mode.width = width;
-        mode.height = height;
-        mode.aspect_ratio[0] = width;
-        mode.aspect_ratio[1] = height;
-
-        if (screen_create_display(&v_display, ctx, &mode))
-        {
-            UNITY_LOG(logger, qh_strerror(errno));
-            return reinterpret_cast<std::uint64_t>(nullptr);
-        }
-
-        return reinterpret_cast<std::uint64_t>(v_display);
+        UNITY_LOG(logger, qh_strerror(errno));
+        return reinterpret_cast<std::uint64_t>(nullptr);
     }
 
-    void UIX DelQNX800VirtualDisplay(std::uint64_t display)
+    // create a buffer
+    screen_buffer_t& buf = buffers[v_display];
+    if (screen_create_buffer(&buf))
     {
-        if (display && screen_destroy_display(reinterpret_cast<screen_display_t>(logger)))
-        {
-            UNITY_LOG(logger, qh_strerror(errno));
-        }
+        UNITY_LOG(logger, qh_strerror(errno));
+        buffers.erase(v_display);
+        return reinterpret_cast<std::uint64_t>(nullptr);
     }
 
-    std::uint64_t UIX ReadQNX800VirtualDisplay(std::uint64_t screen, std::uint32_t gl_tex)
+    if (screen_set_buffer_property_iv(buf, SCREEN_PROPERTY_FORMAT, //
+                                      (const int[]){SCREEN_FORMAT_RGBA8888}))
     {
-        return 0;
+        UNITY_LOG(logger, qh_strerror(errno));
+        buffers.erase(v_display);
+        return reinterpret_cast<std::uint64_t>(nullptr);
+    }
+
+    return reinterpret_cast<std::uint64_t>(v_display);
+}
+
+extern "C" void UIX DelQNX800VirtualDisplay(std::uint64_t display)
+{
+    auto r = buffers.find(reinterpret_cast<screen_display_t>(display));
+    if (r == buffers.end())
+    {
+        std::string msg = "Screen handle " + std::to_string(display) + " does not exist";
+        UNITY_LOG(logger, msg.c_str());
+        return;
+    }
+
+    if (screen_destroy_buffer(r->second))
+    {
+        UNITY_LOG(logger, qh_strerror(errno));
+    }
+
+    if (display && screen_destroy_display(reinterpret_cast<screen_display_t>(display)))
+    {
+        UNITY_LOG(logger, qh_strerror(errno));
+    }
+    buffers.erase(reinterpret_cast<screen_display_t>(display));
+}
+
+extern "C" void UIX ReadQNX800VirtualDisplay(std::uint64_t display,
+                                             std::uint32_t width,
+                                             std::uint32_t height,
+                                             std::uint64_t pixels)
+{
+    auto r = buffers.find(reinterpret_cast<screen_display_t>(display));
+    if (r == buffers.end())
+    {
+        std::string msg = "Screen handle " + std::to_string(display) + " does not exist";
+        UNITY_LOG(logger, msg.c_str());
+        return;
+    }
+
+    void* ptr = reinterpret_cast<screen_display_t>(pixels);
+    int err = 0;
+    // the proper size is width * height * 4 * sizeof(char), by the standard, the sizeof(char) is always 1
+    err |= screen_set_buffer_property_iv(r->second, SCREEN_PROPERTY_BUFFER_SIZE, (const int[]){width, height});
+    err |= screen_set_buffer_property_iv(r->second, SCREEN_PROPERTY_SIZE, (const int[]){width * height * 4});
+    err |= screen_set_buffer_property_iv(r->second, SCREEN_PROPERTY_STRIDE, (const int[]){width * 4});
+    err |= screen_set_buffer_property_pv(r->second, SCREEN_PROPERTY_POINTER, &ptr);
+
+    if (err)
+    {
+        UNITY_LOG(logger, qh_strerror(errno));
+        return;
     }
 }
